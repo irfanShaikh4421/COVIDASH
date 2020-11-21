320aaab2e78a6f1e83a631f139912ffd71155a98
Saish Sankhe <saishsankhe23@gmail.com>
2020-11-21 17:49:51 -0500

    Added testing/:state/:orgID route
    For each testing location

--- Covidash/src/App.js
+++ Covidash/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';//to get bootstrap cards working
+import 'bootstrap/dist/css/bootstrap.min.css'; //to get bootstrap cards working
 //import noImage from './img/imageNotFound.jpg';
-import placeholder from './img/virusPictureFromStatisticsAPI.png'
+import placeholder from './img/virusPictureFromStatisticsAPI.png';
 import Statistics from './components/CasesTracking';
 import Guidelines from './components/Guidelines';
 import HomePage from './components/HomePage';
@@ -12,73 +12,83 @@ import News from './components/News';
 import OutBreak from './components/OutbreakMap';
 import Symptoms from './components/Symptoms';
 import TestingLocations from './components/Testing';
+import EachTestingLocation from './components/EachTestingLocation';
 import Travel from './components/Travel';
 import Vaccine from './components/Vaccine';
 
-
 function App() {
-	
-
 	return (
 		<Router>
-			<div className='App'>
-				<header className='App-header'>
+			<div className="App">
+				<header className="App-header">
 					<br />
-					<img src={placeholder} height="100" width="100"
-						alt='Covidash logo placeholder' title="From the statistics API website" />
-					<h1><Link className='marvel' to='/'>
-						HOME
-					</Link></h1>
+					<img
+						src={placeholder}
+						height="100"
+						width="100"
+						alt="Covidash logo placeholder"
+						title="From the statistics API website"
+					/>
+					<h1>
+						<Link className="marvel" to="/">
+							HOME
+						</Link>
+					</h1>
 					<br />
-					
-					<Link className='marvel' to='/statistics'>
+
+					<Link className="marvel" to="/statistics">
 						Statistics
 					</Link>
-					<Link className='marvel' to='/vaccine'>
+					<Link className="marvel" to="/vaccine">
 						Vaccines
 					</Link>
-          			<Link className='marvel' to='/travel'>
+					<Link className="marvel" to="/travel">
 						TravelRegulations
 					</Link>
-					<Link className='marvel' to='/map'>
+					<Link className="marvel" to="/map">
 						OutbreakMap
 					</Link>
-					<Link className='marvel' to='/symptoms'>
+					<Link className="marvel" to="/symptoms">
 						Symptoms
 					</Link>
-					<Link className='marvel' to='/testing'>
+					<Link className="marvel" to="/testing">
 						TestingLocations
 					</Link>
-					<Link className='marvel' to='/guidelines'>
+					<Link className="marvel" to="/guidelines">
 						Health{'&'}Safety
 					</Link>
-					<Link className='marvel' to='/news'>
+					<Link className="marvel" to="/news">
 						News
 					</Link>
-					<Link className='marvel' to='/login'>
+					<Link className="marvel" to="/login">
 						Login
 					</Link>
 				</header>
 				<br />
 				<br />
-				<div className='App-body'>
+				<div className="App-body">
 					<Switch>
-						<Route exact path='/' component={HomePage} />
-						<Route exact path='/statistics' component={Statistics} />
-						<Route exact path='/vaccine' component={Vaccine} />
-						<Route exact path='/travel' component={Travel} />
-						<Route exact path='/map' component={OutBreak} />
-						<Route exact path='/symptoms' component={Symptoms} />
-						<Route exact path='/testing' component={TestingLocations} />
-						<Route exact path='/guidelines' component={Guidelines} />
-						<Route exact path='/news' component={News} />
-						<Route exact path='/login' component={Login} />
-						<Route render = {() => (<h2>404: Invalid URL</h2>)} />
+						<Route exact path="/" component={HomePage} />
+						<Route exact path="/statistics" component={Statistics} />
+						<Route exact path="/vaccine" component={Vaccine} />
+						<Route exact path="/travel" component={Travel} />
+						<Route exact path="/map" component={OutBreak} />
+						<Route exact path="/symptoms" component={Symptoms} />
+						<Route exact path="/testing" component={TestingLocations} />
+						<Route
+							exact
+							path="/testing/:state/:orgID"
+							component={EachTestingLocation}
+						/>
+						<Route exact path="/guidelines" component={Guidelines} />
+						<Route exact path="/news" component={News} />
+						<Route exact path="/login" component={Login} />
+						<Route render={() => <h2>404: Invalid URL</h2>} />
 					</Switch>
 				</div>
 			</div>
 		</Router>
 	);
-};
+}
 
 export default App;
