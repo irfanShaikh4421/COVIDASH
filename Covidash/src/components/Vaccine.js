401e553ef7db53e51f6f88d3501227a9853011a2
Saish Sankhe <saishsankhe23@gmail.com>
2020-11-21 17:48:36 -0500

    Updated vaccine component

--- Covidash/src/components/Vaccine.js
+++ Covidash/src/components/Vaccine.js
@@ -1,10 +1,42 @@
-//this will contain details about a single comic
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Vaccine = (props) => {
+	const [vaccineData, setVaccineData] = useState([]);
+
+	useEffect(() => {
+		async function getData() {
+			const url = 'https://disease.sh/v3/covid-19/vaccine';
+
+			let response;
+			try {
+				response = await axios.get(url);
+				setVaccineData(response.data.data);
+			} catch (e) {
+				console.log(e);
+			}
+		}
+		getData();
+	}, []);
+
 	return (
-		<p>This is a placeholder for Vaccine Tracking Page</p>
+		<div>
+			{vaccineData.map((item, index) => (
+				<div key={index}>
+					<p>
+						{index + 1}. Sponsors:{' '}
+						{item.sponsors.map((sponsor) => (
+							<span>{sponsor} | </span>
+						))}
+					</p>
+					<p>Mechanism: {item.mechanism}</p>
+					<p>Trial phase: {item.trialPhase}</p>
+					<br />
+					<br />
+				</div>
+			))}
+		</div>
 	);
-}
+};
 
 export default Vaccine;
