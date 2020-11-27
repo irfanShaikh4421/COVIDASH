0e143dcf532527d9e54ecaf0daa94ec4f383407b
Irfan Shaikh <irfanshaikh4421@gmail.com>
2020-11-26 20:30:14 -0500

    Integrated auth on frontend & did minor changes on the backend

--- Covidash/src/App.js
+++ Covidash/src/App.js
@@ -17,80 +17,103 @@ import Travel from './components/Travel';
 import Sources from './components/Sources';
 import Vaccine from './components/Vaccine';
 
-function App() {
+// auth
+import withFirebaseAuth from 'react-with-firebase-auth'
+import { providers, firebaseAppAuth } from './auth/firebase'
+import FirebaseContext from './auth/context'
+
+function App(props) {
 	return (
-		<Router>
-			<div className="App">
-				<header className="App-header">
-					<br />
-					<img
-						src={placeholder}
-						height="100"
-						width="100"
-						alt="Covidash logo placeholder"
-						title="From the statistics API website"
-					/>
-					<h1>
-						<Link className="marvel" to="/">
-							HOME
-						</Link>
-					</h1>
-					<br />
-
-					<Link className="marvel" to="/statistics">
-						Statistics
-					</Link>
-					<Link className="marvel" to="/vaccine">
-						Vaccines
-					</Link>
-					<Link className="marvel" to="/travel">
-						TravelRegulations
-					</Link>
-					<Link className="marvel" to="/map">
-						OutbreakMap
-					</Link>
-					<Link className="marvel" to="/symptoms">
-						Symptoms
-					</Link>
-					<Link className="marvel" to="/testing">
-						TestingLocations
-					</Link>
-					<Link className="marvel" to="/guidelines">
-						Health{'&'}Safety
-					</Link>
-					<Link className="marvel" to="/news">
-						News
-					</Link>
-					<Link className="marvel" to="/login">
-						Login
-					</Link>
-				</header>
-				<br />
-				<br />
-				<div className="App-body">
-					<Switch>
-						<Route exact path="/" component={HomePage} />
-						<Route exact path="/statistics" component={Statistics} />
-						<Route exact path="/vaccine" component={Vaccine} />
-						<Route exact path="/travel" component={Travel} />
-						<Route exact path="/map" component={OutBreak} />
-						<Route exact path="/symptoms" component={Symptoms} />
-						<Route exact path="/testing" component={TestingLocations} />
-						<Route
-							exact
-							path="/testing/:state/:orgID"
-							component={EachTestingLocation}
+		<FirebaseContext.Provider value={props}>
+			<Router>
+				<div className="App">
+					<header className="App-header">
+						<br />
+						<img
+							src={placeholder}
+							height="100"
+							width="100"
+							alt="Covidash logo placeholder"
+							title="From the statistics API website"
 						/>
-						<Route exact path="/guidelines" component={Guidelines} />
-						<Route exact path="/news" component={News} />
-						<Route exact path="/login" component={Login} />
-						<Route exact path='/sources' component={Sources} />
-						<Route render={() => <h2>404: Invalid URL</h2>} />
-					</Switch>
-				</div>
-			</div>
-		</Router>
-	);
-}
+						<h1>
+							<Link className="marvel" to="/">
+								HOME
+							</Link>
+							<p style={{fontSize: "1.5rem", fontWeight: 300, padding: ".5rem"}}>
+								 { props.user ? (<> Hello {props.user.displayName}</>) : null }
+							</p>
+						</h1>
+						<br />
 
-export default App;
+						<Link className="marvel" to="/statistics">
+							Statistics
+						</Link>
+						<Link className="marvel" to="/vaccine">
+							Vaccines
+						</Link>
+						<Link className="marvel" to="/travel">
+							TravelRegulations
+						</Link>
+						<Link className="marvel" to="/map">
+							OutbreakMap
+						</Link>
+						<Link className="marvel" to="/symptoms">
+							Symptoms
+						</Link>
+						<Link className="marvel" to="/testing">
+							TestingLocations
+						</Link>
+						<Link className="marvel" to="/guidelines">
+							Health{'&'}Safety
+						</Link>
+						<Link className="marvel" to="/news">
+							News
+						</Link>
+						<p>
+							{/* Sample  */}
+							<FirebaseContext.Consumer>
+								{firebase => 
+									(<>
+										{ firebase.loading ? (<p>Loading</p>) : null }
+										{ (!firebase.loading && !firebase.user) ? <button className="marvel" onClick={()=> firebase.signInWithGoogle()}> Login with Google </button>  : null }
+										{ !firebase.loading && firebase.user ? (<><button className="marvel" onClick={() => firebase.signOut()}> Sign Out </button></>) : null }
+									</>
+									)
+								} 
+							</FirebaseContext.Consumer>
+						</p>
+					</header>
+					<br />
+					<br />
+					<div className="App-body">
+						<Switch>
+							<Route exact path="/" component={HomePage} />
+							<Route exact path="/statistics" component={Statistics} />
+							<Route exact path="/vaccine" component={Vaccine} />
+							<Route exact path="/travel" component={Travel} />
+							<Route exact path="/map" component={OutBreak} />
+							<Route exact path="/symptoms" component={Symptoms} />
+							<Route exact path="/testing" component={TestingLocations} />
+							<Route
+								exact
+								path="/testing/:state/:orgID"
+								component={EachTestingLocation}
+							/>
+							<Route exact path="/guidelines" component={Guidelines} />
+							<Route exact path="/news" component={News} />
+							<Route exact path="/login" component={Login} />
+							<Route exact path='/sources' component={Sources} />
+							<Route render={() => <h2>404: Invalid URL</h2>} />
+						</Switch>
+					</div>
+				</div>
+			</Router>
+		</FirebaseContext.Provider>
+	);
+}  
+
+export default withFirebaseAuth({
+	providers,
+	firebaseAppAuth,
+})(App);
