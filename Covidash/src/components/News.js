5d16fb68d063c75ef7965713e87c609ae6928daf
Maithili Deshmukh <maithilirssd@gmail.com>
2020-12-06 01:54:55 +0530

    updated news component to fetch tailored results

--- Covidash/src/components/News.js
+++ Covidash/src/components/News.js
@@ -1,40 +1,92 @@
 import React, { useEffect, useState } from 'react';
+import countries from '../data/countries-iso2.json';
 import axios from 'axios';
 import '../App.css';
 
-const News = (props) => {
-    let [newsData, setData] = useState([]);
-    let [newsLoading, setNewsLoading] = useState(true);
+const News = () => {
+    const [country, setCountry] = useState('af');
+    const [newsData, setData] = useState([]);
+    const [tailored, setTailored] = useState(false);
+    const [newsLoading, setNewsLoading] = useState(true);
 
     useEffect(() => {
         async function getData() {
+            const baseUrl = 'https://newsapi.org/v2/top-headlines';
+
+            const q = 'q=covid';
+
+            let countryParam;
+            if (country) {
+                countryParam = country.toLowerCase();
+            }
+
+            const apiKey = 'apiKey=ec4091ce10534690baa4b9f4d5ba7af2';
+
             const url =
-                'https://newsapi.org/v2/top-headlines?q=covid&from=2020-11-10&language=en&sortBy=publishedAt&apiKey=ec4091ce10534690baa4b9f4d5ba7af2&pageSize=100';
-            let getData;
+                baseUrl + '?' + q + '&country=' + countryParam + '&' + apiKey;
+
+            let response;
             try {
-                getData = await axios.get(url);
-                setData(getData.data.articles);
+                response = await axios.get(url);
+
+                if (!response.data.articles.length) {
+                    setTailored(false);
+                    response = await axios.get(
+                        baseUrl + '?' + q + '&' + apiKey
+                    );
+                    setData(response.data.articles);
+                } else {
+                    setTailored(true);
+                    setData(response.data.articles);
+                }
                 setNewsLoading(false);
             } catch (e) {
                 console.log(e);
             }
         }
         getData();
-    }, []);
+    }, [country]);
 
-    if(newsLoading){
+    if (newsLoading) {
         return <div>Loading...</div>;
     }
 
-    console.log(newsData);
+    const handleChange = (e) => {
+        if (e.target.value) setCountry(e.target.value);
+    };
 
     return (
         <div>
-            <h2>Latest News</h2>
+            <label htmlFor="state">Select country &nbsp;&nbsp;</label>
+            <select onChange={handleChange}>
+                {countries.map((country, index) => (
+                    <option key={index} value={country['alpha-2']}>
+                        {country.name}
+                    </option>
+                ))}
+            </select>
+            <br />
+            <br />
+            <h1>Latest News</h1>
+            <br />
+            {tailored ? (
+                <h2>News for your region</h2>
+            ) : (
+                <h2>
+                    Sorry, no news found for your region - here are some top
+                    news for COVID-19
+                </h2>
+            )}
             <br />
             {newsData.map((item, index) => (
                 <div key={index}>
-                    {item.urlToImage && <img src={item.urlToImage} width="300" alt={`${item.source.name} article`}></img>}
+                    {item.urlToImage && (
+                        <img
+                            src={item.urlToImage}
+                            width="300"
+                            alt={`${item.source.name} article`}
+                        ></img>
+                    )}
                     <br />
                     <a className="tota11yLink" href={item.url} target="blank">
                         {item.title}
