0cc1150d21a33426472542a5983aec315f7c44db
Maithili Deshmukh <maithilirssd@gmail.com>
2020-12-06 01:52:46 +0530

    added a static page for health & safety guidelines

--- Covidash/src/components/Guidelines.js
+++ Covidash/src/components/Guidelines.js
@@ -1,9 +1,153 @@
 import React from 'react';
 
-const Guidelines = (props) => {
-	return (
-		<p>This is a placeholder for the Health and Safety Guidelines Page</p>
-	);
-}
+const Guidelines = () => {
+    return (
+        <div>
+            <h1>Things to Know about the COVID-19 Pandemic</h1>
+            <br />
+            <h2>Three Important Ways to Slow the Spread</h2>
+            <br />
+            <p>
+                Wear a mask to protect yourself and others and stop the spread
+                of COVID-19.
+            </p>
+            <p>
+                Stay at least 6 feet (about 2 arm lengths) from others who don’t
+                live with you, particularly in crowded areas.
+            </p>
+            <p>
+                Wash your hands with soap and water for 20 seconds or use hand
+                sanitizer with at least 60% alcohol.
+            </p>
+            <br />
+            <hr />
+            <br />
+            <h2>How to Protect Yourself When Going Out</h2>
+            <br />
+            <p>
+                Wear a mask that covers your nose and mouth to help protect
+                yourself and others. Choose a mask with two or more layers of
+                washable, breathable fabric that fits snugly against the sides
+                of your face.
+            </p>
+            <p>
+                Stay 6 feet apart and avoid crowds. The more people you are in
+                contact with, the more likely you are to be exposed to COVID-19.
+            </p>
+            <p>
+                Avoid indoor spaces as much as possible, particularly ones that
+                aren’t well ventilated. You may find it harder to stay 6 feet
+                apart in indoor spaces.
+            </p>
+            <p>
+                Wash your hands often. Use soap and water for 20 seconds,
+                especially after you have been in a public place or hand
+                sanitizer if soap and water aren’t available.
+            </p>
+            <br />
+            <hr />
+            <br />
+            <h2>If You are at Risk of Getting Very Sick</h2>
+            <br />
+            <p>
+                People of any age can get COVID-19, even healthy young adults
+                and children.
+            </p>
+            <p>
+                People who are older or have certain underlying medical
+                conditions are at higher risk of getting very sick from
+                COVID-19.
+            </p>
+            <p>
+                Other groups may be at higher risk for getting COVID-19 or
+                having more severe illness.
+            </p>
+            <br />
+            <hr />
+            <br />
+            <h2>What to Do If You're Sick</h2>
+            <br />
+            <p>
+                Stay home except to get medical care. Most people with COVID-19
+                have mild illness and recover at home without medical care.
+            </p>
+            <p>
+                Isolate yourself from other members of your family to prevent
+                spread to them and the people that they may have contact with,
+                like grandparents. If you have to leave isolation, wear a mask,
+                stay 6 feet apart from others, wash your hands frequently, and
+                clean all touched surfaces.
+            </p>
+            <p>
+                Even if you don’t feel sick, you can spread COVID-19 to others.
+                Be sure that you protect everyone as though they are the most
+                fragile person in your family.
+            </p>
+            <p>
+                Get care immediately if you are having emergency warning signs,
+                like trouble breathing, pain or pressure in chest.
+            </p>
+            <br />
+            <hr />
+            <br />
+            <h2>How to Get a Test for Current Infection</h2>
+            <br />
+            <p>
+                You can visit your state or local health department’s website to
+                look for the latest local information on testing.
+            </p>
+            <p>
+                If you have symptoms of COVID-19 and want to get tested, call
+                your healthcare provider first.
+            </p>
+            <p>
+                If you have symptoms of COVID-19 and choose to not get tested,
+                it is important to stay home. Find out what to do if you are
+                sick.
+            </p>
+            <br />
+            <hr />
+            <br />
+            <h2>How to Cope with Stress</h2>
+            <br />
+            <p>Care for yourself one small way each day.</p>
+            <p>Unwind by doing yoga, listening to music, or gardening.</p>
+            <p>
+                Find new ways to connect with family and friends, get support,
+                and share feelings.
+            </p>
+            <p>Eat healthy foods and get enough rest</p>
+            <p>
+                Relax by reading, listening to music, or starting a new hobby.
+            </p>
+            <br />
+            <hr />
+            <br />
+            <h2>What Symptoms to Watch For</h2>
+            <br />
+            <h3>The most common symptoms of COVID-19 are</h3>
+            <p>Fever</p>
+            <p>Cough</p>
+            <p>Headaches</p>
+            <p>Fatigue</p>
+            <p>Muscle or body aches</p>
+            <p>Loss of taste or smell</p>
+            <p>Sore throat</p>
+            <p>Nausea</p>
+            <p>Diarrhea</p>
+
+            <br />
+            <hr />
+            <br />
+            <a
+                href="https://www.cdc.gov/coronavirus/2019-ncov/your-health/need-to-know.html"
+                target="_blank"
+            >
+                Source
+            </a>
+            <br />
+        </div>
+    );
+};
 
 export default Guidelines;
