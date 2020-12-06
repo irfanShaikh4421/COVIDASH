fc6341af33790ab5ce418308932e55d08a36f7b0
Saish Sankhe <saishsankhe23@gmail.com>
2020-12-06 09:11:12 +0530

    added more info for each location

--- client/src/components/EachTestingLocation.js
+++ client/src/components/EachTestingLocation.js
@@ -14,7 +14,7 @@ const EachTestingLocation = (props) => {
 			let getData;
 			try {
 				getData = await axios.get(url);
-				/*let hospitalData = */getData.data.forEach((item) => {
+				/*let hospitalData = */ getData.data.forEach((item) => {
 					if (item.id === ID) {
 						setHospitalData(item);
 					}
@@ -25,11 +25,71 @@ const EachTestingLocation = (props) => {
 		}
 		getData();
 	}, [props.match.params.state, props.match.params.orgID]);
+
+	const getAddress = () => {
+		if (hospitalData.physical_address) {
+			return (
+				<p>
+					Address: {hospitalData.physical_address[0].address_1}{' '}
+					{hospitalData.physical_address[0].city}{' '}
+					{hospitalData.physical_address[0].city}{' '}
+					{hospitalData.physical_address[0].state_province}{' '}
+					{hospitalData.physical_address[0].postal_code}
+				</p>
+			);
+		}
+	};
+
+	const getPhone = () => {
+		if (hospitalData.phones) {
+			return (
+				<p>
+					Contact: {hospitalData.phones[0].number} (
+					{hospitalData.phones[0].type})
+				</p>
+			);
+		}
+	};
+
+	const getSchedule = () => {
+		const weekdays = [
+			'Monday',
+			'Tuesday',
+			'Wednesday',
+			'Thursday',
+			'Friday',
+			'Saturday',
+			'Sunday',
+		];
+
+		if (hospitalData.regular_schedule) {
+			const schedule = hospitalData.regular_schedule;
+			if (hospitalData.regular_schedule.length === 0) {
+				return;
+			} else {
+				return (
+					<div>
+						<p>Regular schedule: </p>
+						{schedule.map((item, key) => (
+							<p key={key}>
+								{weekdays[key]}: {item.opens_at} to {item.closes_at}
+							</p>
+						))}
+					</div>
+				);
+			}
+		}
+	};
+
 	return (
 		<div>
-			<h2>{hospitalData.name}</h2><hr />
+			<h2>{hospitalData.name}</h2>
+			<hr />
 			<p>{hospitalData.description}</p>
 			<p>Transportation: {hospitalData.transportation}</p>
+			{getAddress()}
+			{getPhone()}
+			{getSchedule()}
 		</div>
 	);
 };
