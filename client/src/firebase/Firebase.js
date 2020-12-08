5b2448880fabb7aa74ad96156ad91f28c26d37e0
Maithili Deshmukh <maithilirssd@gmail.com>
2020-12-09 02:57:43 +0530

    firebase setup and initialization

---
+++ client/src/firebase/Firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messageingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+});
+
+export default firebaseApp;
---
+++ client/src/firebase/FirebaseFunctions.js
@@ -0,0 +1,43 @@
+import firebase from 'firebase/app';
+
+async function doCreateUserWithEmailAndPassword(email, password, displayName) {
+    await firebase.auth().createUserWithEmailAndPassword(email, password);
+    firebase.auth().currentUser.updateProfile({ displayName: displayName });
+}
+async function doChangePassword(email, oldPassword, newPassword) {
+    let credential = firebase.auth.EmailAuthProvider.credential(
+        email,
+        oldPassword
+    );
+    await firebase.auth().currentUser.reauthenticateWithCredential(credential);
+    await firebase.auth().currentUser.updatePassword(newPassword);
+    await doSignOut();
+}
+async function doSignInWithEmailAndPassword(email, password) {
+    await firebase.auth().signInWithEmailAndPassword(email, password);
+}
+async function doSocialSignIn(provider) {
+    let socialProvider = null;
+    if (provider === 'google') {
+        socialProvider = new firebase.auth.GoogleAuthProvider();
+    } else if (provider === 'facebook') {
+        socialProvider = new firebase.auth.FacebookAuthProvider();
+    }
+
+    await firebase.auth().signInWithPopup(socialProvider);
+}
+async function doPasswordReset(email) {
+    await firebase.auth().sendPasswordResetEmail(email);
+}
+async function doSignOut() {
+    await firebase.auth().signOut();
+}
+
+export {
+    doCreateUserWithEmailAndPassword,
+    doChangePassword,
+    doSignInWithEmailAndPassword,
+    doSocialSignIn,
+    doPasswordReset,
+    doSignOut,
+};
