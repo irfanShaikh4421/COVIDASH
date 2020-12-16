c3abd490d89bd5e212801a434c464bddbcb47b73
Irfan Shaik <irfanshaikh4421@gmail.com>
2020-12-15 19:52:42 -0500

    Implemented imagemagick

--- server-new/routes/index.js
+++ server-new/routes/index.js
@@ -1,7 +1,9 @@
 const newsRoutes = require('./news');
+const photoRoutes = require('./photo');
 
 const constructorMethod = (app) => {
     app.use('/', newsRoutes);
+    app.use('/photo', photoRoutes);
 
     app.use('*', (req, res) => {
         res.json({ error: 404 });
