62839396e0b892d2302ea91a681043b84627a312
Maithili Deshmukh <maithilirssd@gmail.com>
2020-12-18 04:36:51 +0530

    updated account, news and sign out components with new ui

--- client/src/components/Account.js
+++ client/src/components/Account.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState, useContext } from 'react';
 import SignOutButton from './SignOut';
-//import ChangePassword from './ChangePassword';
-//import ChangeCountry from './ChangeUserCountry';
 import { NavLink } from 'react-router-dom';
 import '../App.css';
 import app from 'firebase/app';
@@ -10,359 +8,376 @@ import { AuthContext } from '../firebase/Auth';
 import { LocationContext } from '../LocationContext';
 import countries from '../data/countries.json';
 import states from '../data/usStates.json';
+import { Typography, Button, Image } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
 
 const db = app.firestore();
+
 const countryIndexer = [
-	0,
-	840,
-	356,
-	76,
-	643,
-	250,
-	4,
-	8,
-	12,
-	20,
-	24,
-	660,
-	28,
-	32,
-	51,
-	533,
-	36,
-	40,
-	31,
-	44,
-	48,
-	50,
-	52,
-	112,
-	56,
-	84,
-	204,
-	60,
-	64,
-	68,
-	70,
-	72,
-	92,
-	96,
-	100,
-	854,
-	108,
-	132,
-	116,
-	120,
-	124,
-	535,
-	136,
-	140,
-	148,
-	832,
-	152,
-	156,
-	170,
-	174,
-	178,
-	188,
-	191,
-	192,
-	531,
-	196,
-	203,
-	384,
-	180,
-	208,
-	262,
-	212,
-	214,
-	218,
-	818,
-	222,
-	226,
-	232,
-	233,
-	231,
-	238,
-	234,
-	242,
-	246,
-	254,
-	258,
-	266,
-	270,
-	268,
-	276,
-	288,
-	292,
-	300,
-	304,
-	308,
-	312,
-	320,
-	324,
-	624,
-	328,
-	332,
-	336,
-	340,
-	344,
-	348,
-	352,
-	360,
-	364,
-	368,
-	372,
-	833,
-	376,
-	380,
-	388,
-	392,
-	400,
-	398,
-	404,
-	414,
-	417,
-	418,
-	428,
-	422,
-	426,
-	430,
-	434,
-	438,
-	440,
-	442,
-	446,
-	807,
-	450,
-	454,
-	458,
-	462,
-	466,
-	470,
-	584,
-	474,
-	478,
-	480,
-	175,
-	484,
-	498,
-	492,
-	496,
-	499,
-	500,
-	504,
-	508,
-	104,
-	516,
-	524,
-	528,
-	540,
-	554,
-	558,
-	562,
-	566,
-	578,
-	512,
-	586,
-	275,
-	591,
-	598,
-	600,
-	604,
-	608,
-	616,
-	620,
-	634,
-	642,
-	646,
-	638,
-	410,
-	659,
-	662,
-	663,
-	666,
-	670,
-	674,
-	678,
-	682,
-	686,
-	688,
-	690,
-	694,
-	702,
-	534,
-	703,
-	705,
-	90,
-	706,
-	710,
-	728,
-	724,
-	144,
-	652,
-	729,
-	740,
-	748,
-	752,
-	756,
-	760,
-	158,
-	762,
-	834,
-	764,
-	626,
-	768,
-	780,
-	788,
-	792,
-	796,
-	784,
-	826,
-	800,
-	804,
-	858,
-	860,
-	548,
-	862,
-	704,
-	876,
-	732,
-	887,
-	894,
-	716,
+    0,
+    840,
+    356,
+    76,
+    643,
+    250,
+    4,
+    8,
+    12,
+    20,
+    24,
+    660,
+    28,
+    32,
+    51,
+    533,
+    36,
+    40,
+    31,
+    44,
+    48,
+    50,
+    52,
+    112,
+    56,
+    84,
+    204,
+    60,
+    64,
+    68,
+    70,
+    72,
+    92,
+    96,
+    100,
+    854,
+    108,
+    132,
+    116,
+    120,
+    124,
+    535,
+    136,
+    140,
+    148,
+    832,
+    152,
+    156,
+    170,
+    174,
+    178,
+    188,
+    191,
+    192,
+    531,
+    196,
+    203,
+    384,
+    180,
+    208,
+    262,
+    212,
+    214,
+    218,
+    818,
+    222,
+    226,
+    232,
+    233,
+    231,
+    238,
+    234,
+    242,
+    246,
+    254,
+    258,
+    266,
+    270,
+    268,
+    276,
+    288,
+    292,
+    300,
+    304,
+    308,
+    312,
+    320,
+    324,
+    624,
+    328,
+    332,
+    336,
+    340,
+    344,
+    348,
+    352,
+    360,
+    364,
+    368,
+    372,
+    833,
+    376,
+    380,
+    388,
+    392,
+    400,
+    398,
+    404,
+    414,
+    417,
+    418,
+    428,
+    422,
+    426,
+    430,
+    434,
+    438,
+    440,
+    442,
+    446,
+    807,
+    450,
+    454,
+    458,
+    462,
+    466,
+    470,
+    584,
+    474,
+    478,
+    480,
+    175,
+    484,
+    498,
+    492,
+    496,
+    499,
+    500,
+    504,
+    508,
+    104,
+    516,
+    524,
+    528,
+    540,
+    554,
+    558,
+    562,
+    566,
+    578,
+    512,
+    586,
+    275,
+    591,
+    598,
+    600,
+    604,
+    608,
+    616,
+    620,
+    634,
+    642,
+    646,
+    638,
+    410,
+    659,
+    662,
+    663,
+    666,
+    670,
+    674,
+    678,
+    682,
+    686,
+    688,
+    690,
+    694,
+    702,
+    534,
+    703,
+    705,
+    90,
+    706,
+    710,
+    728,
+    724,
+    144,
+    652,
+    729,
+    740,
+    748,
+    752,
+    756,
+    760,
+    158,
+    762,
+    834,
+    764,
+    626,
+    768,
+    780,
+    788,
+    792,
+    796,
+    784,
+    826,
+    800,
+    804,
+    858,
+    860,
+    548,
+    862,
+    704,
+    876,
+    732,
+    887,
+    894,
+    716,
 ];
 const stateIndexer = [
-	'AL',
-	'AK',
-	'AZ',
-	'AR',
-	'CA',
-	'CO',
-	'CT',
-	'DE',
-	'FL',
-	'GA',
-	'HI',
-	'ID',
-	'IL',
-	'IN',
-	'IA',
-	'KS',
-	'KY',
-	'LA',
-	'ME',
-	'MD',
-	'MA',
-	'MI',
-	'MN',
-	'MS',
-	'MO',
-	'MT',
-	'NE',
-	'NV',
-	'NH',
-	'NJ',
-	'NM',
-	'NY',
-	'NC',
-	'ND',
-	'OH',
-	'OK',
-	'OR',
-	'PA',
-	'RI',
-	'SC',
-	'SD',
-	'TN',
-	'TX',
-	'UT',
-	'VT',
-	'VA',
-	'WA',
-	'WV',
-	'WI',
-	'WY',
+    'AL',
+    'AK',
+    'AZ',
+    'AR',
+    'CA',
+    'CO',
+    'CT',
+    'DE',
+    'FL',
+    'GA',
+    'HI',
+    'ID',
+    'IL',
+    'IN',
+    'IA',
+    'KS',
+    'KY',
+    'LA',
+    'ME',
+    'MD',
+    'MA',
+    'MI',
+    'MN',
+    'MS',
+    'MO',
+    'MT',
+    'NE',
+    'NV',
+    'NH',
+    'NJ',
+    'NM',
+    'NY',
+    'NC',
+    'ND',
+    'OH',
+    'OK',
+    'OR',
+    'PA',
+    'RI',
+    'SC',
+    'SD',
+    'TN',
+    'TX',
+    'UT',
+    'VT',
+    'VA',
+    'WA',
+    'WV',
+    'WI',
+    'WY',
 ];
 
 function Account() {
-	const [userData, setUserData] = useState({});
-	const { currentUser } = useContext(AuthContext);
-	const [location] = useContext(LocationContext);
-	const uid = currentUser.uid;
+    const [userData, setUserData] = useState({});
+    const { currentUser } = useContext(AuthContext);
+    const [location] = useContext(LocationContext);
+    const uid = currentUser.uid;
 
-	useEffect(() => {
-		async function getUserData() {
-			var docRef = db.collection('users').doc(uid);
+    const { Title } = Typography;
 
-			docRef
-				.get()
-				.then(function (doc) {
-					if (doc.exists) {
-						console.log('Document data:', doc.data());
-						setUserData(doc.data());
-					} else {
-						// doc.data() will be undefined in this case
-						console.log('No such document!');
-					}
-				})
-				.catch(function (error) {
-					console.log('Error getting document:', error);
-				});
-		}
-		getUserData();
-	}, [uid]);
+    useEffect(() => {
+        async function getUserData() {
+            var docRef = db.collection('users').doc(uid);
 
-	const userInfo = () => {
-		if (location.countryCode === 840) {
-			const stateName = states[stateIndexer.indexOf(location.state)].name;
-			console.log(stateIndexer.indexOf(location.state));
-			console.log(states[stateIndexer.indexOf(location.state)]);
-			console.log(states[stateIndexer.indexOf(location.state)].name);
-			console.log(location);
-			console.log(stateName);
-			return <p>Location: {stateName}, United States of America</p>;
-		} else {
-			const countryName =
-				countries[countryIndexer.indexOf(location.countryCode)].country;
-			return <p>Location: {countryName}</p>;
-		}
-	};
+            docRef
+                .get()
+                .then(function (doc) {
+                    if (doc.exists) {
+                        console.log('Document data:', doc.data());
+                        setUserData(doc.data());
+                    } else {
+                        // doc.data() will be undefined in this case
+                        console.log('No such document!');
+                    }
+                })
+                .catch(function (error) {
+                    console.log('Error getting document:', error);
+                });
+        }
+        getUserData();
+    }, [uid]);
 
-	return (
-		<div className="Account">
-			<h2>Account details</h2>
-			<img src={userData.imageUrl} alt="profile icon" width="200px" />
-			<p>Name: {currentUser.displayName}</p>
-			<p>Email: {currentUser.email}</p>
-			{userInfo()}
-			<NavLink
-				exact
-				to="/user-details"
-				activeClassName="active"
-				className="marvel"
-			>
-				Change location
-			</NavLink>
-			<NavLink
-				exact
-				to="/upload-image"
-				activeClassName="active"
-				className="marvel"
-			>
-				Change profile picture
-			</NavLink>
-			<NavLink
-				exact
-				to="/change-password"
-				activeClassName="active"
-				className="marvel"
-			>
-				Change password
-			</NavLink>
-			<SignOutButton />
-		</div>
-	);
+    const userInfo = () => {
+        if (location.countryCode === 840) {
+            const stateName = states[stateIndexer.indexOf(location.state)].name;
+            return (
+                <div>
+                    <span className="sub-heading">Location:</span>
+                    <span className="sub-info">
+                        {stateName}, United States of America
+                    </span>
+                </div>
+            );
+        } else {
+            const countryName =
+                countries[countryIndexer.indexOf(location.countryCode)].country;
+            return (
+                <div>
+                    <span className="sub-heading">Location:</span>
+                    <span className="sub-info">{countryName}</span>
+                </div>
+            );
+        }
+    };
+
+    return (
+        <div className="Account">
+            <Title>Account details</Title>
+            <Image
+                width={200}
+                height={200}
+                src={
+                    userData.imageUrl
+                        ? userData.imageUrl
+                        : 'https://via.placeholder.com/300.jpg?text=Profile+Image'
+                }
+                placeholder={<LoadingOutlined className="loader" />}
+                alt="Profile icon"
+                className="profile-img"
+            ></Image>
+            <span className="sub-heading">Name:</span>
+            <span className="sub-info">{currentUser.displayName}</span>
+            <span className="sub-heading">Email:</span>
+            <span className="sub-info">{currentUser.email}</span>
+            {userInfo()}
+
+            <Button className="btn-right-margin">
+                <NavLink exact to="/user-details" activeClassName="active">
+                    Change location
+                </NavLink>
+            </Button>
+            <Button className="btn-right-margin">
+                <NavLink exact to="/upload-image" activeClassName="active">
+                    Change profile picture
+                </NavLink>
+            </Button>
+            <Button className="btn-right-margin">
+                <NavLink exact to="/change-password" activeClassName="active">
+                    Change password
+                </NavLink>
+            </Button>
+            <SignOutButton />
+        </div>
+    );
 }
 
 export default Account;
--- client/src/components/News.js
+++ client/src/components/News.js
@@ -7,382 +7,401 @@ import { LoadingOutlined } from '@ant-design/icons';
 import { Select, Card, Row, Col, Typography, Image, Empty } from 'antd';
 
 const iso2Indexer = [
-	4,
-	248,
-	8,
-	12,
-	16,
-	20,
-	24,
-	660,
-	10,
-	28,
-	32,
-	51,
-	533,
-	36,
-	40,
-	31,
-	44,
-	48,
-	50,
-	52,
-	112,
-	56,
-	84,
-	204,
-	60,
-	64,
-	68,
-	535,
-	70,
-	72,
-	74,
-	76,
-	86,
-	96,
-	100,
-	854,
-	108,
-	132,
-	116,
-	120,
-	124,
-	136,
-	140,
-	148,
-	152,
-	156,
-	162,
-	166,
-	170,
-	174,
-	178,
-	180,
-	184,
-	188,
-	384,
-	191,
-	192,
-	531,
-	196,
-	203,
-	208,
-	262,
-	212,
-	214,
-	218,
-	818,
-	222,
-	226,
-	232,
-	233,
-	748,
-	231,
-	238,
-	234,
-	242,
-	246,
-	250,
-	254,
-	258,
-	260,
-	266,
-	270,
-	268,
-	276,
-	288,
-	292,
-	300,
-	304,
-	308,
-	312,
-	316,
-	320,
-	831,
-	324,
-	624,
-	328,
-	332,
-	334,
-	336,
-	340,
-	344,
-	348,
-	352,
-	356,
-	360,
-	364,
-	368,
-	372,
-	833,
-	376,
-	380,
-	388,
-	392,
-	832,
-	400,
-	398,
-	404,
-	296,
-	408,
-	410,
-	414,
-	417,
-	418,
-	428,
-	422,
-	426,
-	430,
-	434,
-	438,
-	440,
-	442,
-	446,
-	450,
-	454,
-	458,
-	462,
-	466,
-	470,
-	584,
-	474,
-	478,
-	480,
-	175,
-	484,
-	583,
-	498,
-	492,
-	496,
-	499,
-	500,
-	504,
-	508,
-	104,
-	516,
-	520,
-	524,
-	528,
-	540,
-	554,
-	558,
-	562,
-	566,
-	570,
-	574,
-	807,
-	580,
-	578,
-	512,
-	586,
-	585,
-	275,
-	591,
-	598,
-	600,
-	604,
-	608,
-	612,
-	616,
-	620,
-	630,
-	634,
-	638,
-	642,
-	643,
-	646,
-	652,
-	654,
-	659,
-	662,
-	663,
-	666,
-	670,
-	882,
-	674,
-	678,
-	682,
-	686,
-	688,
-	690,
-	694,
-	702,
-	534,
-	703,
-	705,
-	90,
-	706,
-	710,
-	239,
-	728,
-	724,
-	144,
-	729,
-	740,
-	744,
-	752,
-	756,
-	760,
-	158,
-	762,
-	834,
-	764,
-	626,
-	768,
-	772,
-	776,
-	780,
-	788,
-	792,
-	795,
-	796,
-	798,
-	800,
-	804,
-	784,
-	826,
-	840,
-	581,
-	858,
-	860,
-	548,
-	862,
-	704,
-	92,
-	850,
-	876,
-	732,
-	887,
-	894,
-	716,
+    4,
+    248,
+    8,
+    12,
+    16,
+    20,
+    24,
+    660,
+    10,
+    28,
+    32,
+    51,
+    533,
+    36,
+    40,
+    31,
+    44,
+    48,
+    50,
+    52,
+    112,
+    56,
+    84,
+    204,
+    60,
+    64,
+    68,
+    535,
+    70,
+    72,
+    74,
+    76,
+    86,
+    96,
+    100,
+    854,
+    108,
+    132,
+    116,
+    120,
+    124,
+    136,
+    140,
+    148,
+    152,
+    156,
+    162,
+    166,
+    170,
+    174,
+    178,
+    180,
+    184,
+    188,
+    384,
+    191,
+    192,
+    531,
+    196,
+    203,
+    208,
+    262,
+    212,
+    214,
+    218,
+    818,
+    222,
+    226,
+    232,
+    233,
+    748,
+    231,
+    238,
+    234,
+    242,
+    246,
+    250,
+    254,
+    258,
+    260,
+    266,
+    270,
+    268,
+    276,
+    288,
+    292,
+    300,
+    304,
+    308,
+    312,
+    316,
+    320,
+    831,
+    324,
+    624,
+    328,
+    332,
+    334,
+    336,
+    340,
+    344,
+    348,
+    352,
+    356,
+    360,
+    364,
+    368,
+    372,
+    833,
+    376,
+    380,
+    388,
+    392,
+    832,
+    400,
+    398,
+    404,
+    296,
+    408,
+    410,
+    414,
+    417,
+    418,
+    428,
+    422,
+    426,
+    430,
+    434,
+    438,
+    440,
+    442,
+    446,
+    450,
+    454,
+    458,
+    462,
+    466,
+    470,
+    584,
+    474,
+    478,
+    480,
+    175,
+    484,
+    583,
+    498,
+    492,
+    496,
+    499,
+    500,
+    504,
+    508,
+    104,
+    516,
+    520,
+    524,
+    528,
+    540,
+    554,
+    558,
+    562,
+    566,
+    570,
+    574,
+    807,
+    580,
+    578,
+    512,
+    586,
+    585,
+    275,
+    591,
+    598,
+    600,
+    604,
+    608,
+    612,
+    616,
+    620,
+    630,
+    634,
+    638,
+    642,
+    643,
+    646,
+    652,
+    654,
+    659,
+    662,
+    663,
+    666,
+    670,
+    882,
+    674,
+    678,
+    682,
+    686,
+    688,
+    690,
+    694,
+    702,
+    534,
+    703,
+    705,
+    90,
+    706,
+    710,
+    239,
+    728,
+    724,
+    144,
+    729,
+    740,
+    744,
+    752,
+    756,
+    760,
+    158,
+    762,
+    834,
+    764,
+    626,
+    768,
+    772,
+    776,
+    780,
+    788,
+    792,
+    795,
+    796,
+    798,
+    800,
+    804,
+    784,
+    826,
+    840,
+    581,
+    858,
+    860,
+    548,
+    862,
+    704,
+    92,
+    850,
+    876,
+    732,
+    887,
+    894,
+    716,
 ];
 
 const News = () => {
-	const [location] = useContext(LocationContext);
-	const [newsData, setNewsData] = useState([]);
-	const [tailored, setTailored] = useState(false);
-	const [newsLoading, setNewsLoading] = useState(true);
+    const [location] = useContext(LocationContext);
+    const [newsData, setNewsData] = useState([]);
+    const [tailored, setTailored] = useState(false);
+    const [newsLoading, setNewsLoading] = useState(true);
 
-	console.log(newsData);
+    console.log(newsData);
 
-	const { Option } = Select;
-	const { Title } = Typography;
+    const { Option } = Select;
+    const { Title } = Typography;
 
-	function findCountry(countryId) {
-		//countryID should be passed as location context.countryCode
-		let countryIndex = null;
-		if (countryId === 0) {
-			return 'zz'; //'zz' signifies that world news is the originally desired return data
-		}
-		countryIndex = iso2Indexer.indexOf(countryId); //getting the index of the Iso2 in iso2.json using countryId
-		if (countryIndex === -1) {
-			//if not in iso2.json, then default to the World
-			return 'aa'; //this will result in an output of world news, but 'aa' specificly will signify that the desired data was not originally the world data (and is not contained in the iso2)
-		} else {
-			return countries[countryIndex]['alpha-2']; //the user's preferred  country's iso2
-		}
-	}
+    function findCountry(countryId) {
+        //countryID should be passed as location context.countryCode
+        let countryIndex = null;
+        if (countryId === 0) {
+            return 'zz'; //'zz' signifies that world news is the originally desired return data
+        }
+        countryIndex = iso2Indexer.indexOf(countryId); //getting the index of the Iso2 in iso2.json using countryId
+        if (countryIndex === -1) {
+            //if not in iso2.json, then default to the World
+            return 'aa'; //this will result in an output of world news, but 'aa' specificly will signify that the desired data was not originally the world data (and is not contained in the iso2)
+        } else {
+            return countries[countryIndex]['alpha-2']; //the user's preferred  country's iso2
+        }
+    }
 
-	const [country, setCountry] = useState(findCountry(location.countryCode));
+    const [country, setCountry] = useState(findCountry(location.countryCode));
 
-	useEffect(() => {
-		async function getNews() {
-			setNewsLoading(true);
+    useEffect(() => {
+        async function getNews() {
+            setNewsLoading(true);
 
-			let response = await axios.get(`/news/${country}`);
-			let data = response.data.news;
-			setNewsData(data);
+            let response = await axios.get(`/news/${country}`);
+            let data = response.data.news;
+            setNewsData(data);
 
-			if (response.data.tailored === true) setTailored(true);
-			else setTailored(false);
-			setNewsLoading(false);
-		}
-		getNews();
-	}, [country]);
+            if (response.data.tailored === true) setTailored(true);
+            else setTailored(false);
+            setNewsLoading(false);
+        }
+        getNews();
+    }, [country]);
 
-	if (newsLoading) {
-		return <LoadingOutlined className="loader" />;
-	}
+    if (newsLoading) {
+        return <LoadingOutlined className="loader" />;
+    }
 
-	const handleChange = (value) => {
-		if (value) setCountry(value);
-	};
+    const handleChange = (value) => {
+        if (value) setCountry(value);
+    };
 
-	return (
-		<div>
-			<Title>News</Title>
-			<label>
-				Select country: &nbsp;&nbsp;
-				<Select
-					showSearch
-					style={{ width: 300 }}
-					placeholder={country ? country : 'Planet Earth'}
-					optionFilterProp="children"
-					onChange={handleChange}
-					value={country}
-					filterOption={(input, option) =>
-						option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-					}
-				>
-					<Option value="zz">Planet Earth</Option>
-					{countries.map((country, index) => (
-						<Option key={index} value={country['alpha-2']}>
-							{country.name}
-						</Option>
-					))}
-				</Select>
-			</label>
-			<div>
-				{newsData.length === 0 ? (
-					<span className="news-header">
-						Sorry, no recent news found for the selected region
-						<Empty />
-					</span>
-				) : tailored ? (
-					<span className="news-header">Covid news for selected region</span>
-				) : country === 'zz' ? (
-					<span className="news-header">Covid news from everywhere</span>
-				) : (
-					<span className="news-header">
-						Sorry, no recent news found for the selected region - here is the
-						top news for COVID-19 from around the world
-					</span>
-				)}
+    return (
+        <div>
+            <Title>News</Title>
+            <label>
+                Select country: &nbsp;&nbsp;
+                <Select
+                    showSearch
+                    style={{ width: 300 }}
+                    placeholder={country ? country : 'Planet Earth'}
+                    optionFilterProp="children"
+                    onChange={handleChange}
+                    value={country}
+                    filterOption={(input, option) =>
+                        option.children
+                            .toLowerCase()
+                            .indexOf(input.toLowerCase()) >= 0
+                    }
+                >
+                    <Option value="zz">Planet Earth</Option>
+                    {countries.map((country, index) => (
+                        <Option key={index} value={country['alpha-2']}>
+                            {country.name}
+                        </Option>
+                    ))}
+                </Select>
+            </label>
+            <div>
+                {newsData.length === 0 ? (
+                    <span className="news-header">
+                        Sorry, no recent news found for the selected region
+                        <Empty />
+                    </span>
+                ) : tailored ? (
+                    <span className="news-header">
+                        Covid news for selected region
+                    </span>
+                ) : country === 'zz' ? (
+                    <span className="news-header">
+                        Covid news from everywhere
+                    </span>
+                ) : (
+                    <span className="news-header">
+                        Sorry, no recent news found for the selected region -
+                        here is the top news for COVID-19 from around the world
+                    </span>
+                )}
 
-				<Row gutter={[40, 30]}>
-					{newsData.map((item, index) => (
-						<Col className="gutter-row" sm={24} md={12} key={index}>
-							<div key={index}>
-								<a href={item.url} target="blank" className="card-link">
-									<Card
-										hoverable
-										className="news-card"
-										cover={
-											<Image
-												alt={`${item.source.name} article`}
-												src={item.urlToImage}
-												placeholder={<LoadingOutlined className="img-loader" />}
-												preview={false}
-											/>
-										}
-									>
-										<div className="card-content">
-											<span className="card-title">{item.title}</span>
-											<span className="card-date">
-												{new Date(item.publishedAt).toLocaleString('en-US')}
-											</span>
-											<span className="card-body">{item.description}</span>
-										</div>
-									</Card>
-								</a>
-							</div>
-						</Col>
-					))}
-				</Row>
-			</div>
-		</div>
-	);
+                <Row gutter={[40, 30]}>
+                    {newsData.map((item, index) => (
+                        <Col className="gutter-row" sm={24} md={12} key={index}>
+                            <div key={index}>
+                                <a
+                                    href={item.url}
+                                    target="blank"
+                                    className="card-link"
+                                >
+                                    <Card
+                                        hoverable
+                                        className="news-card"
+                                        cover={
+                                            <Image
+                                                alt={`${item.source.name} article`}
+                                                src={item.urlToImage}
+                                                placeholder={
+                                                    <LoadingOutlined className="img-loader" />
+                                                }
+                                                preview={false}
+                                                fallback="https://via.placeholder.com/300.jpg?text=No+Image"
+                                            />
+                                        }
+                                    >
+                                        <div className="card-content">
+                                            <span className="card-title">
+                                                {item.title}
+                                            </span>
+                                            <span className="card-date">
+                                                {new Date(
+                                                    item.publishedAt
+                                                ).toLocaleString('en-US')}
+                                            </span>
+                                            <span className="card-body">
+                                                {item.description}
+                                            </span>
+                                        </div>
+                                    </Card>
+                                </a>
+                            </div>
+                        </Col>
+                    ))}
+                </Row>
+            </div>
+        </div>
+    );
 };
 
 export default News;
