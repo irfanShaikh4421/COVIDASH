e82fc7b492f4355f2ba015e17f78b51853c102e2
Maithili Deshmukh <maithilirssd@gmail.com>
2020-12-18 04:27:41 +0530

    updated signup and signin with new ui

--- client/src/components/SocialSignIn.js
+++ client/src/components/SocialSignIn.js
@@ -1,30 +1,38 @@
 import React from 'react';
 import { doSocialSignIn } from '../firebase/FirebaseFunctions';
 import '../App.css';
+import { Button } from 'antd';
+import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons';
 
 const SocialSignIn = () => {
-	const socialSignOn = async (provider) => {
-		try {
-			await doSocialSignIn(provider);
-		} catch (e) {
-			alert(e);
-		}
-	};
+    const socialSignOn = async (provider) => {
+        try {
+            await doSocialSignIn(provider);
+        } catch (e) {
+            alert(e);
+        }
+    };
 
-	return (
-		<div>
-			<img
-				onClick={() => socialSignOn('google')}
-				alt="google sign in"
-				src="/imgs/btn_google_signin.png"
-			/>
-			<img
-				onClick={() => socialSignOn('facebook')}
-				alt="facebook sign in"
-				src="/imgs/facebook_signin.png"
-			/>
-		</div>
-	);
+    return (
+        <div>
+            <Button
+                onClick={() => socialSignOn('google')}
+                alt="google sign in"
+                icon={<GoogleOutlined />}
+                className="social-signup-btn"
+            >
+                Sign up with Google
+            </Button>
+            <Button
+                onClick={() => socialSignOn('facebook')}
+                alt="facebook sign in"
+                icon={<FacebookOutlined />}
+                className="social-signup-btn"
+            >
+                Sign up with Facebook
+            </Button>
+        </div>
+    );
 };
 
 export default SocialSignIn;
