8dd9870216e24e5954e10089e4da942114cd0683
Maithili Deshmukh <maithilirssd@gmail.com>
2020-12-18 02:25:11 +0530

    fixed the map container width

--- client/src/components/OutbreakMap.js
+++ client/src/components/OutbreakMap.js
@@ -4,32 +4,37 @@ import Legend from './Legend';
 import LoadCountriesTask from '../tasks/LoadCountriesTask';
 import legendItems from '../entities/LegendItems';
 import { LoadingOutlined } from '@ant-design/icons';
+import { Typography } from 'antd';
 
 const OutBreak = () => {
-	const [countries, setCountries] = useState([]);
-	const legendItemsRev = [...legendItems].reverse();
+    const [countries, setCountries] = useState([]);
+    const legendItemsRev = [...legendItems].reverse();
 
-	const load = () => {
-		let loadCountriesTask = new LoadCountriesTask();
-		loadCountriesTask.load((countries) => setCountries(countries));
-	};
+    const { Title, Text } = Typography;
 
-	useEffect(load, []);
+    const load = () => {
+        let loadCountriesTask = new LoadCountriesTask();
+        loadCountriesTask.load((countries) => setCountries(countries));
+    };
 
-	return (
-		<div>
-			<h2>OutBreak Map</h2>
-			<br />
-			{countries.length === 0 ? (
-				<LoadingOutlined className="loader" />
-			) : (
-				<div>
-					<CovidMap countries={countries} />
-					<Legend legendItems={legendItemsRev} />
-				</div>
-			)}
-		</div>
-	);
+    useEffect(load, []);
+
+    return (
+        <div className="full-width flex-column">
+            <Title>Outbreak map</Title>
+            <Text className="symptom-header">
+                Click on each country to see the statistics
+            </Text>
+            {countries.length === 0 ? (
+                <LoadingOutlined className="loader" />
+            ) : (
+                <div>
+                    <CovidMap countries={countries} />
+                    <Legend legendItems={legendItemsRev} />
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default OutBreak;
