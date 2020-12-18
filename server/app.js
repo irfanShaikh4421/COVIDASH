350cce16ed96db197ce366514c033953d8e280c5
Saish Sankhe <saishsankhe23@gmail.com>
2020-12-18 06:18:24 +0530

    renamed server-new to server

--- server/app.js
+++ server/app.js
@@ -1,47 +1,18 @@
-const express = require('express')
-const app = express()
-const { redisClient } = require('./utilities/redis')
-const cors = require('cors')
-const admin = require('firebase-admin')
-const { authenticate } = require('./config/firebase')
+const express = require('express');
+const app = express();
+const static = express.static(__dirname + '/public');
 
-// environment configs if any
-require('dotenv').config()
+const configRoutes = require('./routes');
 
-//default configs
-const config = require("./config/index")
+app.use('/public', static);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cors())
-app.use(express.json({ limit: "50kb" }))
-app.use(express.urlencoded({ extended : true }))
-app.use(express.static('./public'))
+const port = 5000;
 
+configRoutes(app);
 
-// routes
-const test = require('./routes/test')
-const authRoute = require('./routes/authRoute')
-
-app.use("/test", test)
-app.use("/authRoute", authenticate ,authRoute)
-
-// auth thingy
-
-// global error handler
-
-app.use((err,req,res,next) => {
-
-    if( err.response && err.response.data.status)
-        err.status = err.response.data.status
-
-    if(!err.status)
-    {
-        err.status = 500
-        err.message = "INTERNAL SERVER ERROR"
-    }
-    console.error(err)
-    res.status(err.status).json(err.message)
-} )
-
-app.listen(config.port, () => {
-    console.log(`[BACKEND] Your server is running on ${config.port}`)
-})
+app.listen(port, () => {
+    console.log("We've now got a server!");
+    console.log(`Your routes will be running on http://localhost:${port}`);
+});
